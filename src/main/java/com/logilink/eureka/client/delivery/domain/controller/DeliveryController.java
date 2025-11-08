4e3a0ca7da1c301fe73d365b352767d4eeb8080a
jjaenie <wogus990519@gmail.com>
2025-11-09 01:33:23 +0900

    refactor (delivery) : 배송 생성 요청 dto 이름 변경 반영 (#1)

--- src/main/java/com/logilink/eureka/client/delivery/domain/controller/DeliveryController.java
+++ src/main/java/com/logilink/eureka/client/delivery/domain/controller/DeliveryController.java
@@ -2,9 +2,8 @@ package com.logilink.eureka.client.delivery.domain.controller;
 
 import com.logilink.eureka.client.delivery.common.BaseResponse;
 import com.logilink.eureka.client.delivery.domain.model.Delivery;
-import com.logilink.eureka.client.delivery.domain.model.dto.RequestDto;
+import com.logilink.eureka.client.delivery.domain.model.dto.CreateRequestDto;
 import com.logilink.eureka.client.delivery.domain.service.DeliveryService;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +19,8 @@ public class DeliveryController {
 
     // Todo. 게이트웨이 체인에 orderId 넣어주는지, 아님 프론트에서 넘겨주는걸로 가도 되는지 확인
     @PostMapping("/deliveries/{orderId}")
-    public BaseResponse createDelivery(@PathVariable UUID orderId, @RequestBody RequestDto requestDto) {
-        Delivery result = deliveryService.createDelivery(orderId, requestDto);
+    public BaseResponse createDelivery(@PathVariable UUID orderId, @RequestBody CreateRequestDto createRequestDto) {
+        Delivery result = deliveryService.createDelivery(orderId, createRequestDto);
         return BaseResponse.success(result);
     }
 
