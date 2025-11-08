13718355f9841d235c66b2bd66fd69b5db5c8dc0
jjaenie <wogus990519@gmail.com>
2025-11-09 04:45:16 +0900

    feat (delivery) : 배송 현황 검색 응답 dto 생성 (#3)

---
+++ src/main/java/com/logilink/eureka/client/delivery/domain/model/dto/SearchDeliveryResponseDto.java
@@ -0,0 +1,15 @@
+package com.logilink.eureka.client.delivery.domain.model.dto;
+
+import com.logilink.eureka.client.delivery.common.constants.DeliveryStatus;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@AllArgsConstructor
+public class SearchDeliveryResponseDto {
+    private UUID deliveryId;
+    private DeliveryStatus status;
+    private UUID orderId;
+}
