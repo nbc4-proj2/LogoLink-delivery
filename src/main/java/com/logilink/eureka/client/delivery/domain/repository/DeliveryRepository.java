2c861e0656755dc2cb9036ec58908ebb54db09ee
jjaenie <wogus990519@gmail.com>
2025-11-09 04:46:19 +0900

    feat (delivery) : 배송 조회 및 검색 함수 추가 (#3)

--- src/main/java/com/logilink/eureka/client/delivery/domain/repository/DeliveryRepository.java
+++ src/main/java/com/logilink/eureka/client/delivery/domain/repository/DeliveryRepository.java
@@ -1,9 +1,14 @@
 package com.logilink.eureka.client.delivery.domain.repository;
 
 import com.logilink.eureka.client.delivery.domain.model.Delivery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
+    Page<Delivery> findAllByDeletedAtIsNull(Pageable pageable);
+    List<Delivery> findAllByOrderIdAndDeletedAtIsNullOrderByCreatedAtAsc(UUID orderId);
 }
