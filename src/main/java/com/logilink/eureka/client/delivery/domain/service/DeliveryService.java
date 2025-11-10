ec4db8ff3726911e216330eb8d8bbc99666f40eb
jjaenie <wogus990519@gmail.com>
2025-11-10 11:57:47 +0900

    refactor (delivery) : Service에서 페이지로 목록 조회, 배송 삭제 시 엔티티 반환 -> dto 반환으로 리팩토링

--- src/main/java/com/logilink/eureka/client/delivery/domain/service/DeliveryService.java
+++ src/main/java/com/logilink/eureka/client/delivery/domain/service/DeliveryService.java
@@ -3,6 +3,8 @@ package com.logilink.eureka.client.delivery.domain.service;
 import com.logilink.eureka.client.delivery.common.constants.DeliveryStatus;
 import com.logilink.eureka.client.delivery.common.exception.AppException;
 import com.logilink.eureka.client.delivery.common.exception.DeliveryErrorCode;
+import com.logilink.eureka.client.delivery.domain.model.dto.responseDto.DeleteResponseDto;
+import com.logilink.eureka.client.delivery.domain.model.dto.responseDto.ResponseDto;
 import com.logilink.eureka.client.delivery.domain.model.entity.Delivery;
 import com.logilink.eureka.client.delivery.domain.model.dto.requestDto.CreateRequestDto;
 import com.logilink.eureka.client.delivery.domain.model.dto.responseDto.SearchDeliveryResponseDto;
@@ -28,19 +30,20 @@ public class DeliveryService {
 
     // 배송 생성
     @Transactional
-    public Delivery createDelivery(UUID orderId, CreateRequestDto createRequestDto) {
+    public ResponseDto createDelivery(UUID orderId, CreateRequestDto createRequestDto) {
         // Todo. 권한 검증 : 마스터 관리자만 생성 가능, 여기서 검증 하는지 게이트웨이에서 하는지 둘 다 인지
         // Todo. orderId 검증
 
         Delivery delivery = createRequestDto.toDelivery();
         delivery.setOrderId(orderId);
         deliveryRepository.save(delivery);
-        return delivery;
+        ResponseDto responseDto = new ResponseDto(delivery);
+        return responseDto;
     }
 
     // 배송 수정
     @Transactional
-    public Delivery updateDelivery(UUID deliveryId, UpdateRequestDto updateRequestDto) {
+    public ResponseDto updateDelivery(UUID deliveryId, UpdateRequestDto updateRequestDto) {
         // Todo. 권한 : 마스터, 허브관리자, 배송담당자
 
         Delivery delivery = validDeliveryId(deliveryId);
@@ -53,37 +56,26 @@ public class DeliveryService {
             delivery.setDeliveryManagerId(updateRequestDto.getDeliveryManagerId());
         }
 
-        return  delivery;
-    }
-
-    // 배송 삭제
-    @Transactional
-    public Delivery deleteDelivery(UUID deliveryId, Long userId) {
-        // Todo. 권한 : 마스터, 허브관리자 <- userId
-
-        Delivery delivery = validDeliveryId(deliveryId);
-
-        delivery.softDelete(LocalDateTime.now(), userId);
-
-        return delivery;
+        return new ResponseDto(delivery);
     }
 
     // 배송 단건 조회
     @Transactional(readOnly = true)
-    public Delivery getDelivery(UUID deliveryId) {
+    public ResponseDto getDelivery(UUID deliveryId) {
         Delivery delivery = validDeliveryId(deliveryId);
 
         if(delivery.getDeletedAt() != null) {
             throw AppException.of(DeliveryErrorCode.DELIVERY_IS_NOT_EXISTING);
         }
 
-        return delivery;
+        return new ResponseDto(delivery);
     }
 
     // 배송 목록 조회
     @Transactional(readOnly = true)
-    public Page<Delivery> getDeliveryList(Pageable pageable) {
-        return vaildDeliveryPage(pageable);
+    public Page<ResponseDto> getDeliveryPage(Pageable pageable) {
+        Page<Delivery> deliveryPage = vaildDeliveryPage(pageable);
+        return deliveryPage.map(ResponseDto::new);
     }
 
     // 배송 현황 검색
@@ -105,6 +97,18 @@ public class DeliveryService {
             responseDtoList.add(new SearchDeliveryResponseDto(deliveryId, status, orderId));
         }
         return responseDtoList;
+    }
+
+    // 배송 삭제
+    @Transactional
+    public DeleteResponseDto deleteDelivery(UUID deliveryId, Long userId) {
+        // Todo. 권한 : 마스터, 허브관리자 <- userId
+
+        Delivery delivery = validDeliveryId(deliveryId);
+
+        delivery.softDelete(LocalDateTime.now(), userId);
+
+        return new DeleteResponseDto(delivery);
     }
 
     // 배송 상태 변경 전 검증 메서드 :  배송완료 상태이면 변경 불가
